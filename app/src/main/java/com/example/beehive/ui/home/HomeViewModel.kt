59b8f360f5f10be0bdf6410cde282cb10f70633e
Roberto Neil Santos <rdsantos21@up.edu.ph>
2024-07-10 22:38:22 +0800

    implemented search feature

--- app/src/main/java/com/example/beehive/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/example/beehive/ui/home/HomeViewModel.kt
@@ -4,23 +4,49 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.beehive.data.Password
 import com.example.beehive.data.PasswordsRepository
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.flow.update
 
 class HomeViewModel(private val passwordsRepository: PasswordsRepository) : ViewModel() {
+    private val _homeUiState = MutableStateFlow(HomeUiState())
     val homeUiState: StateFlow<HomeUiState> =
-        passwordsRepository.getAllPasswordsStream().map { HomeUiState(passwords = it) }
-            .stateIn(
-                scope = viewModelScope,
-                started = SharingStarted.WhileSubscribed(5_000),
-                initialValue = HomeUiState()
+        combine(
+            _homeUiState.map { it.query },
+            passwordsRepository.getAllPasswordsStream()
+        ) { query, passwords ->
+            val filteredPasswords = if (query.isBlank()) {
+                passwords
+            } else {
+                passwords.filter {
+                    it.site.contains(query, ignoreCase = true)
+                }
+            }
+            HomeUiState(
+                passwords = filteredPasswords,
+                query = query
             )
+        }.stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5000),
+            initialValue = HomeUiState()
+        )
+
+    fun onQueryChange(query: String) {
+        _homeUiState.update { currentState ->
+            currentState.copy(
+                query = query
+            )
+        }
+    }
 
     suspend fun deletePassword(id: Int) {
         passwordsRepository.deletePassword(id)
     }
 }
 
-data class HomeUiState(val passwords: List<Password> = listOf())
+data class HomeUiState(val passwords: List<Password> = listOf(), var query: String = "")
