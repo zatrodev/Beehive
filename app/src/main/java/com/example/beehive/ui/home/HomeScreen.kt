f76e36e04a45a628d05aa651e8800d80c7c5df76
Roberto Neil Santos <rdsantos21@up.edu.ph>
2024-07-25 23:13:54 +0800

    added no password description

--- app/src/main/java/com/example/beehive/ui/home/HomeScreen.kt
+++ app/src/main/java/com/example/beehive/ui/home/HomeScreen.kt
@@ -3,23 +3,27 @@ package com.example.beehive.ui.home
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Add
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.beehive.R
-import com.example.beehive.data.Password
+import com.example.beehive.data.passwords.Password
 import com.example.beehive.ui.BeehiveViewModelProvider
+import com.example.beehive.ui.Dimensions.LargePadding
 import com.example.beehive.ui.Dimensions.SmallPadding
 import com.example.beehive.ui.common.BeehiveButton
 import com.example.beehive.ui.home.components.PasswordsGrid
@@ -37,7 +41,10 @@ fun HomeScreen(
     val coroutineScope = rememberCoroutineScope()
     val homeUiState by viewModel.homeUiState.collectAsStateWithLifecycle()
 
-    Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+    Scaffold(
+        modifier = Modifier
+            .fillMaxSize()
+    ) { innerPadding ->
         HomeContent(
             passwords = homeUiState.passwords,
             query = homeUiState.query,
@@ -67,29 +74,52 @@ fun HomeContent(
     modifier: Modifier = Modifier
 ) {
     Column(
-        horizontalAlignment = Alignment.CenterHorizontally,
         modifier = modifier
             .navigationBarsPadding()
+
     ) {
-        SearchBar(
-            query = query,
-            onValueChanged = { onQueryChange(it) },
+        Spacer(modifier = Modifier.height(LargePadding))
+        Text(
+            text = "Beehive",
+            style = MaterialTheme.typography.headlineLarge,
+            modifier = Modifier.padding(
+                LargePadding
+            )
         )
-        PasswordsGrid(
-            passwords = passwords,
-            onDelete = onDeletePassword,
-            onEdit = navigateToEditPassword,
-            sharedElementTransition = sharedElementTransition,
-        )
-        Spacer(modifier = Modifier.weight(1f))
-        BeehiveButton(
-            text = stringResource(R.string.add_password_button),
-            containerColor = MaterialTheme.colorScheme.secondary,
-            contentColor = MaterialTheme.colorScheme.onSecondary,
-            icon = Icons.Outlined.Add,
-            onClick = onAddPasswordClick,
-            modifier = Modifier.padding(SmallPadding)
-        )
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            SearchBar(
+                query = query,
+                onValueChanged = { onQueryChange(it) },
+            )
+            if (passwords.isEmpty()) {
+                Spacer(modifier = Modifier.weight(0.6f))
+                Text(
+                    text = stringResource(R.string.no_password_description),
+                    style = MaterialTheme.typography.displayMedium.copy(
+                        textAlign = TextAlign.Center
+                    ),
+                    color = MaterialTheme.colorScheme.surfaceVariant,
+                )
+            } else {
+                PasswordsGrid(
+                    passwords = passwords,
+                    onDelete = onDeletePassword,
+                    onEdit = navigateToEditPassword,
+                    sharedElementTransition = sharedElementTransition,
+                )
+            }
+            Spacer(modifier = Modifier.weight(1f))
+            BeehiveButton(
+                text = stringResource(R.string.add_password_button),
+                containerColor = MaterialTheme.colorScheme.primary,
+                contentColor = MaterialTheme.colorScheme.onPrimary,
+                icon = Icons.Outlined.Add,
+                onClick = onAddPasswordClick,
+                modifier = Modifier.padding(SmallPadding)
+            )
+        }
     }
 }
 
