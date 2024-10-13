8d57f840a19cd749b2a1e207df8f8cd7233f9282
Roberto Neil Santos <rdsantos21@up.edu.ph>
2024-10-13 20:49:10 +0800

    added scroll

--- app/src/main/java/com/example/beehive/ui/credential/add/AddCredentialScreen.kt
+++ app/src/main/java/com/example/beehive/ui/credential/add/AddCredentialScreen.kt
@@ -10,6 +10,8 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
@@ -109,7 +111,10 @@ private fun AddCredentialContent(
         onPasswordChange(generatePassword(sliderPosition, checkboxStates))
     }
 
-    Column(verticalArrangement = Arrangement.spacedBy(MediumPadding), modifier = modifier) {
+    Column(
+        verticalArrangement = Arrangement.spacedBy(MediumPadding),
+        modifier = modifier.verticalScroll(rememberScrollState())
+    ) {
         Row(
             verticalAlignment = Alignment.CenterVertically,
             horizontalArrangement = Arrangement.SpaceBetween,
--- app/src/main/java/com/example/beehive/ui/credential/edit/EditCredentialScreen.kt
+++ app/src/main/java/com/example/beehive/ui/credential/edit/EditCredentialScreen.kt
@@ -13,6 +13,8 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
@@ -162,7 +164,7 @@ private fun EditCredentialContent(
     }
 
     Column(
-        modifier = modifier,
+        modifier = modifier.verticalScroll(rememberScrollState()),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.spacedBy(MediumPadding)
     ) {
